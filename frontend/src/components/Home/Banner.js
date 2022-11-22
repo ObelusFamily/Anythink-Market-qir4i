6c5b8f52e9d85d1bdbd43d19be31ab76b28bff5b
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-11-21 23:21:53 -0600

    Fix formatting with prettier

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -8,7 +8,9 @@ const Banner = () => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div className="row align-items-center">
-          <span id="get-part" className="h3">A Place to get</span>
+          <span id="get-part" className="h3">
+            A Place to get
+          </span>
           <SearchBox />
           <span className="h3">the cool stuff</span>
         </div>
--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,11 +1,7 @@
-import React from "react"
+import React from "react";
 import { connect } from "react-redux";
 import agent from "../agent";
 import { APPLY_TITLE_FILTER } from "../constants/actionTypes";
-
-const mapStateToProps = (state) => ({
-  ...state.itemList,
-});
 
 const mapDispatchToProps = (dispatch) => ({
   onSearch: (title, pager, payload) =>
@@ -13,35 +9,36 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const SearchBox = (props) => {
-  const handleSearch = (ev) => { 
+  const handleSearch = (ev) => {
     const query = ev.target.value;
-    if(query.length > 3) {
-      props.onSearch(query,
+    if (query.length > 3) {
+      props.onSearch(
+        query,
         (page) => agent.Items.byTitle(query, page),
-        agent.Items.byTitle(query),
+        agent.Items.byTitle(query)
       );
     }
-  }
+  };
 
   return (
     <div className="input-group col">
-      <input 
-      id="search-box"
-      type="text"
-      className="form-control form-control-lg p-4"
-      placeholder="What is it that you truly desire?"
-      onChange={handleSearch}
+      <input
+        id="search-box"
+        type="text"
+        className="form-control form-control-lg p-4"
+        placeholder="What is it that you truly desire?"
+        onChange={handleSearch}
       />
       <div className="input-group-append">
-        <button 
-        className="btn btn-outline-light bg-white border-0"
-        onClick={handleSearch}
+        <button
+          className="btn btn-outline-light bg-white border-0"
+          onClick={handleSearch}
         >
           <i className="ion-search h3"></i>
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SearchBox);
+export default connect(() => ({}), mapDispatchToProps)(SearchBox);
