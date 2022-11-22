6fe2da28612e406cad83439a0831249849fc8c2e
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-11-22 16:59:52 -0600

    Show/Hide SearchBox when clicking "get"

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import SearchBox from "../SearchBox";
 
 const Banner = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const toggle = () => setShowSearch(!showSearch);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div className="row align-items-center">
-          <span id="get-part" className="h3">
-            A Place to get
+        <div className="row align-items-center justify-content-center">
+          <span className="h3 p-1">A Place to </span>
+          <span id="get-part" className="h3 pr-1" onClick={toggle}>
+            get
           </span>
-          <SearchBox />
-          <span className="h3">the cool stuff</span>
+          {showSearch && <SearchBox />}
+          <span className="h3 pl-1">the cool stuff</span>
         </div>
       </div>
     </div>
