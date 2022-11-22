6e6db1c3eebb0c2cc0edb9ab51403e5c711d3579
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-11-21 22:38:27 -0600

    Add SearchBox component

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { connect } from "react-redux";
+import agent from "../agent";
+import { APPLY_TITLE_FILTER } from "../constants/actionTypes";
+
+const mapStateToProps = (state) => ({
+  ...state.itemList,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onSearch: (title, pager, payload) =>
+    dispatch({ type: APPLY_TITLE_FILTER, title, pager, payload }),
+});
+
+const SearchBox = ({ onSearch }) => {
+  const handleSearch = (ev) => { 
+    const query = ev.target.value;
+    if(query.length > 3) {
+      onSearch(query, (page) => agent.Items.byTitle(query, page), agent.Items.byTitle(query));
+    }
+  }
+
+  return (
+    <div className="input-group col">
+      <input 
+      id="search-box"
+      type="text"
+      className="form-control form-control-lg p-4"
+      placeholder="What is it that you truly desire?"
+      onChange={handleSearch}
+      />
+      <div className="input-group-append">
+        <button className="btn btn-outline-light bg-white border-0" onClick={handleSearch}>
+          <i className="ion-search h3"></i>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(SearchBox);
